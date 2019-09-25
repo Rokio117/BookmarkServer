d6af8aa68e5cc59f2a2f0236b6f1ea487a29bde7
Nick Hillary <hillan02@gmail.com>
2019-09-25 16:16:31 -0500

    hopefully ready to go

---
+++ src/getBookmarks/bookmarks.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const uuid = require('uuid');
+const logger = require('../logger');
+const { bookmarks } = require('../store');
+const getRouter = express.Router();
+
+getRouter.route('/').get((req, res) => {
+  res.send('Hello World');
+});
+
+modlule.exports = getRouter;
