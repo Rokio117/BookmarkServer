49b1f5b64dcb3caa48c33c3038212801bba99e8e
Nick Hillary <hillan02@gmail.com>
2019-09-25 16:29:35 -0500

    save all

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(logger);
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
+  console.log(process.env.API_TOKEN);
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
--- src/getBookmarks/bookmarks.js
+++ src/getBookmarks/bookmarks.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const uuid = require('uuid');
 const logger = require('../logger');
-const { bookmarks } = require('../store');
+//const { bookmarks } = require('../store');
 const getRouter = express.Router();
 
 getRouter.route('/').get((req, res) => {
   res.send('Hello World');
 });
 
-modlule.exports = getRouter;
+module.exports = getRouter;
