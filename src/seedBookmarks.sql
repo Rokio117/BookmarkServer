ae86191546c619f7b768bf024ee40151761c9fb2
Nick Hillary <hillan02@gmail.com>
2019-10-03 16:49:06 -0500

    SAVE ALL

---
+++ src/seedBookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+('Big','www.big.com','such big very wow', 5),
+('Shok','www.shok.com','big shok much very so', 4),
+('Quick math','www.quickMath.com','2+2=4-1 thats 3 QUICK MATH', 3),
+('O tah eesh knee','www.otah.com','It doesnt have to make sense', 4),
+('ting go shrkrkroseoraaaah','www.tingoesskra.com','papa ka ka ka', 5)
